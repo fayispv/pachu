8e236fa343667404c7223a5cdc0ab2de6c95f104
Pachuz <fayispv2016@gmail.com>
2024-01-21 16:04:12 +0530

    modified to commit again

--- hello.py
+++ hello.py
@@ -5,3 +5,11 @@ def factorial(number):
   return result
 
 print(factorial(5))
+
+def factorial_rec(number):
+  if number >1 :
+    res = number * factorial_rec(number-1)
+    return res
+  return 1
+  
+print(factorial_rec(6))
